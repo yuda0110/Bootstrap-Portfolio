638f4a39d510fd590cee6a7d948e095673dfb3a2
Yuko Uda <yuko.uda0110@gmail.com>
2020-02-21 13:05:54 -0800

    Use js for creating content of the header and make active class work for the nav links

--- assets/js/header-footer.js
+++ assets/js/header-footer.js
@@ -5,19 +5,37 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const createHeader = () => {
   const header = document.querySelector('.header')
-  const headerHtml = `<div class="header-contents">
-            <div class="name">Yuko Uda</div>
-            <nav>
-                <ul class="navigation">
-                    <li><a href="./index.html">About</a></li>
-                    <li><a href="./portfolio.html">Portfolio</a></li>
-                    <li><a href="./contact.html">Contact</a></li>
+  const headerHtml = `<nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand name" href="./index.html">Yuko Uda</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="./index.html">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link nav-portfolio" href="./portfolio.html">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./contact.html">Contact</a>
+                    </li>
                 </ul>
-            </nav>
-        </div>`
+            </div>
+        </nav>`
   if (header) {
     header.innerHTML = headerHtml
+    activeNavLink('.nav-link')
   }
+}
+
+const activeNavLink = (navLinkClass) => {
+  document.querySelectorAll(navLinkClass).forEach((navLink) => {
+    if (location.href.includes(navLink.getAttribute('href').split("/").pop())) {
+      navLink.classList.add('active')
+    }
+  })
 }
 
 const createFooter = () => {
